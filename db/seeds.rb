f81711d29b9cd66f1d12092ab11aaddfc69cae2b
xiang.ning.liu@oracle.com <xiangnl@slc09pns.us.oracle.com>
2017-03-11 01:49:39 -0800

    deploy ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Service.create!(admin_user: 'XIANG.NING.LIU@ORACLE.COM', domain_name: 'hzcec1714new0203', doc_service_name: 'hzcec0203', osn_service_name: 'instance510246437', service_created_at: '2017-02-03 10:16:45', is_terminated: false, instances_per_account: 1)
